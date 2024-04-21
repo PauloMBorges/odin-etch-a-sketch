c8056ebf6eac92d3467fe95812993b051c93f43c
PauloMBorges <paulodemeloborges@gmail.com>
2024-04-21 11:29:17 -0300

    Add hover effect to change square color in grid
    - Added a `changeSquareColor` function to change the background color of squares to blue when hovered over.
    - Attached a 'mouseover' event listener to the grid container to handle color changes using event delegation. This approach ensures that any square hovered over within the container triggers the color change.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Function to create the grid 
+// Function to create the grid
 function createGrid() {
     const gridSize = 16; /* 16x16 grid */
     const totalSquares = gridSize * gridSize;
@@ -12,8 +12,13 @@ function createGrid() {
         square.classList.add("singleSquare");
         container.appendChild(square);
     }
+
+    container.addEventListener('mouseover', changeSquareColor);
 }
 
 // Event listener to create the grid when the DOM content is loaded
 document.addEventListener("DOMContentLoaded", createGrid);
 
+function changeSquareColor(event) {
+    event.target.style.backgroundColor = 'blue';
+}
